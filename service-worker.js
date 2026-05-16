e9698005f9d5ac18032a379516014d75d6f7d7cb
wesselaerts <wessel.aerts@ziggo.nl>
2026-05-16 16:11:43 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Forge PWA service worker
 // Bump CACHE_VERSION wanneer je updates uitrolt zodat clients vernieuwen
-const CACHE_VERSION = 'forge-v2';
+const CACHE_VERSION = 'forge-v4';
 
 const ASSETS = [
   './',
